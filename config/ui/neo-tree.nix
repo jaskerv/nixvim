d498add7013f3eee27c1378dbb4b8921d7d05f24
jono <jono2496@gmail.com>
2024-08-26 06:19:38 -0700

    neotree keymaps

--- config/ui/neo-tree.nix
+++ config/ui/neo-tree.nix
@@ -2,13 +2,28 @@
   plugins.neo-tree = {
     enable = true;
   };
-
   keymaps = [
-    # General maps
     {
       mode = "n";
       key = "\\";
-      action = "<cmd>Neotree toggle right<cr>";
+      action = "<cmd>Neotree source=filesystem toggle right<cr>";
     }
+    {
+      mode = "n";
+      key = "<leader>ng";
+      action = "<cmd>Neotree source=git_status toggle float<cr>";
+      options = {
+        desc = "Neotree git status";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>nb";
+      action = "<cmd>Neotree source=buffers toggle float<cr>";
+      options = {
+        desc = "Neotree buffers";
+      };
+    }
+
   ];
 }
